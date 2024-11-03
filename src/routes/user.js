f8328837d686fce1e178265cc75c036f7049b6dc
Hermmann Blanco Serrano <32681730+Hermmann@users.noreply.github.com>
2024-11-02 23:06:48 -0500

    delete user completed

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,25 +7,10 @@ const UserController = require('../controllers/user');
 const router = express.Router();
 
 //We create a route for the methods in our controllers
-
-router.get ('/users/user', UserController.getUser);
-router.post('/users', UserController.createUser);
-router.get('/users', UserController.getNumberUser);
-
-module.exports = router;
-const express = require('express');
-
-//Here we upload the controllers
-const UserController = require('../controllers/user');
-
-//Here we call the router
-const router = express.Router();
-
-//We create a route for the methods in our controllers
 router.get ('/users/user', UserController.getUser);
 router.post('/users', UserController.createUser);
 router.get('/users', UserController.getNumberUser);
 router.put('/users/:id', UserController.updateUser);
-
+router.delete('/users/:id', UserController.deleteUser);
 
 module.exports = router;
